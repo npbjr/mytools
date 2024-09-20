b45fc61b80a11b34d5f44f3d855396d5922df006
npbjr <normanbanzonjr@gmail.com>
2024-09-21 01:04:31 +0800

    remove some new lines

--- application/util/decorators.py
+++ application/util/decorators.py
@@ -2,7 +2,6 @@ from typing import Any, Callable, Type
 import json
 from functools import wraps
 
-
 class CustomJsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, str):
@@ -10,19 +9,15 @@ class CustomJsonEncoder(json.JSONEncoder):
 
         return super().default(obj)
 
-
 def firehose(payload: object): ...
 def log_incomming_request(f: Callable[..., Any]): ...
 def log_incomming_response(f: Callable[..., Any]): ...
-
 def handle_request(func): ...
-
 def handle_response(cfunc: Callable[..., Any]):
     """
     the reason for this is i wanted to pass a function as parameter to this decorator
     any custom function that can modify the data before handling it in the decorator
     """
-
     def wrapper_function(func, *args, **kwargs):
         @wraps(func)
         def inner(*args, **kwargs):
@@ -38,7 +33,5 @@ def handle_response(cfunc: Callable[..., Any]):
             # well i need to follow what my reponse_handler wants, i will update this in the future
             # so will convert it back to str
             return json.dumps(func_result)
-
         return inner
-
     return wrapper_function
