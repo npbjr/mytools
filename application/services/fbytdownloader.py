1ee80b4107700bc0e0aaf58444b69647eeb30e24
npbjr <normanbanzonjr@gmail.com>
2024-09-22 08:28:52 +0800

    rename and refactor

--- application/services/fbytdownloader.py
+++ application/services/fbytdownloader.py
@@ -1,6 +1,6 @@
 from flask import send_file, after_this_request
 from typing import Callable, Any
-import yt_dlp
+import yt_dlp as video
 import json
 import os
 from datetime import datetime
@@ -10,8 +10,7 @@ class FBYTDownloader:
     """
     please dont mind with the exagerating code, i need this to stay active on python :)
     """
-
-    def __init__(self): ...
+    def __init__(self):...
 
     def __metadata__(self, f: Callable[[dict], Any]):
         f = f()
@@ -20,33 +19,34 @@ class FBYTDownloader:
     def download_mp3(self, link: str) -> json: ...
     def download_mp4_720p(self, link: str) -> json: ...
     def download_mp4_1024p(self, link: str) -> json: ...
+
     def download_mp4(self, meta: Callable[[dict], Any], link: str) -> json:
 
         filename, downloads_folder = self.__metadata__(meta)
 
         @after_this_request
-        def cleanup(response):
+        def clean_directory(response):
             """
             this is will remove the file in your linux system
             """
             os.remove(filename)
+
             print(f"---- Success Deleting of file {filename} --- ")
-            
+
             return response
-
-        ydl_opts = {
-            "format": "best", 
-            "outtmpl": os.path.join(
-                downloads_folder,
-                filename,
-            ) 
-        }
+            
         try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([link])
+            with video.YoutubeDL({
+                    "format": "best", 
+                    "outtmpl": os.path.join(
+                        downloads_folder,
+                        filename,
+                    ) 
+                }) as vid:
+                vid.download([link])
 
-            info_d = ydl.extract_info(link, download=False)
-            filename = ydl.prepare_filename(info_d)
+            info_d = vid.extract_info(link, download=False)
+            filename = vid.prepare_filename(info_d)
             return send_file(filename, as_attachment=True)
         
         except Exception as e:
